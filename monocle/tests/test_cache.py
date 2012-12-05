4281a2312a9e2517cb0b7f464c4dce72ef35d590
Shaun Duncan <shaun.duncan@coxinc.com>
2012-12-05 16:42:45 -0500

    Fixed a test

--- monocle/tests/test_cache.py
+++ monocle/tests/test_cache.py
@@ -10,15 +10,19 @@ class TestCache(TestCase):
     def test_get_or_prime_primes(self):
         cache.delete('foo')
 
+        # Check that we primed
         cached, primed = get_or_prime('foo', primer='bar')
-
         assert primed
+        assert cached == 'bar'
+
+        # Subsequent call should not prime
+        cached, primed = get_or_prime('foo', primer='baz')
+        assert not primed
         assert cached == 'bar'
 
     def test_get_or_prime_does_not_prime(self):
         cache.set('foo', 'bar')
 
         cached, primed = get_or_prime('foo', primer='baz')
-
         assert not primed
-        assert cached != 'baz'
+        assert cached == 'bar'
