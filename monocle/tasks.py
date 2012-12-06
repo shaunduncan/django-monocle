7231111b63a56b2defff9012747d93d38f6787f3
Shaun Duncan <shaun.duncan@coxinc.com>
2012-12-06 14:37:26 -0500

    Make fetch external task a class

--- monocle/tasks.py
+++ monocle/tasks.py
@@ -1,7 +1,8 @@
 import json
 import urllib2
 
-from celery.task import task
+from celery import registry
+from celery.task import Task
 
 from monocle.cache import cache
 from monocle.resources import Resource
@@ -9,43 +10,49 @@ from monocle.settings import settings
 from monocle.util import extract_content_url
 
 
-@task(queue=settings.TASK_QUEUE,
-      ignore_result=True,
-      max_retries=settings.TASK_EXTERNAL_MAX_RETRIES,
-      default_retry_delay=settings.TASK_EXTERNAL_RETRY_DELAY)
-def request_external_oembed(url):
+class RequestExternalOembedTask(Task)
     """
-    Obtains an oembed resource from an external provider with
+    Fetch an oembed resource from an external provider with
     sensible task retry. Results are expected to be valid JSON
     meaning the url provided must explicitly contain format=json
     """
-    logger = request_external_oembed.get_logger()
-    logger.info('Requesting OEmbed Resource %s' % url)
+    name = 'request_external_oembed'
+    ignore_result = True
+    queue = settings.TASK_QUEUE
+    max_retries = settings.TASK_EXTERNAL_MAX_RETRIES
+    default_retry_delay = settings.TASK_EXTERNAL_RETRY_DELAY
 
-    try:
-        request = urllib2.urlopen(url, timeout=settings.HTTP_TIMEOUT)
-    except urllib2.HTTPError, e:
-        logger.error('Failed to obtain %s : Status %s' % (url, e.code))
-    except urllib2.URLError, e:
-        if 'timed out' in str(e):
-            # On a timeout, retry in hopes that it won't next time
-            request_external_oembed.retry(args=[url], exc=e)
-        else:
-            logger.exception('Unexeped error when retrieving OEmbed %s' % url)
-    else:
-        if request.getcode() != 200:
-            logger.error('URL %s resulted in unexpected HTTP status' % (url, request.getcode()))
-        else:
-            original_url = extract_content_url(url)
+    def run(self, url):
+        logger = self.get_logger()
+        logger.info('Requesting OEmbed Resource %s' % url)
 
-            try:
-                # TODO: Any validation that should happen here?
-                # Do we store invalid data? If invalid do we clear the cache?
-                resource = Resource(original_url, json.loads(request.read()))
-            except ValueError:
-                logger.error('OEmbed response from %s contains invalid JSON' % url)
+        try:
+            request = urllib2.urlopen(url, timeout=settings.HTTP_TIMEOUT)
+        except urllib2.HTTPError, e:
+            logger.error('Failed to obtain %s : Status %s' % (url, e.code))
+        except urllib2.URLError, e:
+            if 'timed out' in str(e):
+                # On a timeout, retry in hopes that it won't next time
+                self.retry(args=[url], exc=e)
             else:
-                # Update the cache with this data
-                cache.set(url, resource)
-            finally:
-                request.close()
+                logger.exception('Unexeped error when retrieving OEmbed %s' % url)
+        else:
+            if request.getcode() != 200:
+                logger.error('URL %s resulted in unexpected HTTP status' % (url, request.getcode()))
+            else:
+                original_url = extract_content_url(url)
+
+                try:
+                    # TODO: Any validation that should happen here?
+                    # Do we store invalid data? If invalid do we clear the cache?
+                    resource = Resource(original_url, json.loads(request.read()))
+                except ValueError:
+                    logger.error('OEmbed response from %s contains invalid JSON' % url)
+                else:
+                    # Update the cache with this data
+                    cache.set(url, resource)
+                finally:
+                    request.close()
+
+
+request_external_oembed = registry.tasks[RequestExternalOEmbedTask.name]
