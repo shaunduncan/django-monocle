65bd6c7899b1bda35e96a1c4fc067d254c2430a9
Shaun Duncan <shaun.duncan@coxinc.com>
2013-01-02 14:34:26 -0500

    DRY up settings using __getattr__

--- monocle/settings.py
+++ monocle/settings.py
@@ -6,6 +6,57 @@ class Settings(object):
     Monocle settings exposed as object properties to allow for dynamic
     settings support
     """
+    _SETTINGS_PREFIX = 'MONOCLE_'
+    _DEFAULTS = {
+        # Raise warning if internal providers do not respect max width/height
+        'RESOURCE_CHECK_INTERNAL_SIZE': False,
+
+        # Default dimensions for internal providers that don't specify sizes
+        'RESOURCE_DEFAULT_DIMENSIONS': [(x, x) for x in xrange(100, 1000, 100)],
+
+        # Minimum TTL for OEmbed resource to be considered fresh (in seconds)
+        'RESOURCE_MIN_TTL': 60*60,
+
+        # Default TTL for OEmbed resource to be considered fresh (in seconds)
+        'RESOURCE_DEFAULT_TTL': 60*60*24*7,
+
+        # Should rendered resources be URLized if they are invalid
+        'RESOURCE_URLIZE_INVALID': True,
+
+        # Should local provider resources be cached
+        'CACHE_INTERNAL_PROVIDERS': False,
+
+        # Should local providers be exposed by default
+        'EXPOSE_LOCAL_PROVIDERS': True,
+
+        # Default timeout in seconds for external HTTP requests
+        'HTTP_TIMEOUT': 3,
+
+        # Celery queue for monocle tasks
+        'TASK_QUEUE': 'monocle',
+
+        # Delay between retries for async external request tasks
+        'TASK_EXTERNAL_RETRY_DELAY': 1,
+
+        # Max number of retries for async external request tasks
+        'TASK_EXTERNAL_MAX_RETRIES': 3,
+
+        # Prefix string for monocle cached objects
+        'CACHE_KEY_PREFIX': 'MONOCLE',
+
+        # Default cache age
+        'CACHE_AGE': 60*60*24*30,
+    }
+
+    def __getattr__(self, attr):
+        """
+        Django settings access with fallback to preconfigured defaults.
+        """
+        if attr in self._DEFAULTS:
+            django_key = '%s%s' % (self._SETTINGS_PREFIX, attr)
+            return getattr(_settings, django_key, self._DEFAULTS[attr])
+        else:
+            raise AttributeError
 
     @property
     def RESOURCE_TYPES(self):
@@ -29,82 +80,6 @@ class Settings(object):
             'title', 'author_name', 'author_url', 'cache_age', 'provider_name',
             'provider_url', 'thumbnail_url', 'thumbnail_width', 'thumbnail_height'
         )
-
-    @property
-    def RESOURCE_CHECK_INTERNAL_SIZE(self):
-        """
-        Checks internal provider returned data to ensure maxwidth/maxheight respected.
-        Raises a warning if they are not respected.
-        """
-        return getattr(_settings, 'MONOCLE_RESOURCE_CHECK_INTERNAL_SIZE', False)
-
-    @property
-    def RESOURCE_DEFAULT_DIMENSIONS(self):
-        """
-        Default dimensions for internal providers if implementations do not
-        define valid sizes.
-        """
-        return getattr(_settings, 'MONOCLE_RESOURCE_DEFAULT_DIMENSIONS',
-                       [(x, x) for x in xrange(100, 1000, 100)])
-
-    @property
-    def RESOURCE_MIN_TTL(self):
-        """Minimum TTL for OEmbed resource to be considered fresh (in seconds)"""
-        # 1 Hour Default
-        return getattr(_settings, 'MONOCLE_RESOURCE_MIN_TTL', 60*60)
-
-    @property
-    def RESOURCE_DEFAULT_TTL(self):
-        """Default TTL for OEmbed resource to be considered fresh (in seconds)"""
-        # 1 Week Default
-        return getattr(_settings, 'MONOCLE_RESOURCE_DEFAULT_TTL', 60*60*24*7)
-
-    @property
-    def RESOURCE_URLIZE_INVALID(self):
-        """Should rendered resources bet URLized if they are invalid"""
-        return getattr(_settings, 'MONOCLE_RESOURCE_URLIZE_INVALID', True)
-
-    @property
-    def CACHE_INTERNAL_PROVIDERS(self):
-        """Setting to indicate if local providers should cache resources"""
-        return getattr(_settings, 'MONOCLE_CACHE_INTERNAL_PROVIDERS', False)
-
-    @property
-    def EXPOSE_LOCAL_PROVIDERS(self):
-        """Should local providers be exposed to external consumers"""
-        return getattr(_settings, 'MONOCLE_EXPOSE_LOCAL_PROVIDERS', True)
-
-    @property
-    def HTTP_TIMEOUT(self):
-        """Default timeout in seconds for external HTTP requests"""
-        # 3 Second Default
-        return getattr(_settings, 'MONOCLE_HTTP_TIMEOUT', 3)
-
-    @property
-    def TASK_QUEUE(self):
-        """Celery queue to use for monocle tasks"""
-        return getattr(_settings, 'MONOCLE_TASK_QUEUE', 'monocle')
-
-    @property
-    def TASK_EXTERNAL_RETRY_DELAY(self):
-        """Delay between retries for async external request task"""
-        # 1 Seconds Default
-        return getattr(_settings, 'MONOCLE_TASK_EXTERNAL_RETRY_DELAY', 1)
-
-    @property
-    def TASK_EXTERNAL_MAX_RETRIES(self):
-        """Maximum number of retries for async external request task"""
-        return getattr(_settings, 'MONOCLE_TASK_EXTERNAL_MAX_RETRIES', 3)
-
-    @property
-    def CACHE_KEY_PREFIX(self):
-        """Prefix string for monocle cached objects"""
-        return getattr(_settings, 'MONOCLE_CACHE_KEY_PREFIX', 'MONOCLE')
-
-    @property
-    def CACHE_AGE(self):
-        # 30 Day Default
-        return getattr(_settings, 'MONOCLE_CACHE_AGE', 60*60*24*30)
 
     @property
     def CACHE_BACKEND(self):
