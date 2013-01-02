cfbac87b21ce0c238d6ca1f28f071021f1ea7088
Shaun Duncan <shaun.duncan@coxinc.com>
2013-01-02 13:12:33 -0500

    Updates for clarity. prefetch() now uses one consumer per call

--- monocle/consumers.py
+++ monocle/consumers.py
@@ -15,13 +15,11 @@ class Consumer(object):
     # From https://github.com/worldcompany/djangoembed/blob/master/oembed/constants.py#L43
     url_regex = re.compile(r'(https?://[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_|])', re.I)
 
-    def __init__(self, maxwidth=None, maxheight=None, skip_internal=False):
-        self.maxwidth = maxwidth
-        self.maxheight = maxheight
+    def __init__(self, skip_internal=False):
         self.skip_internal = skip_internal
         registry.ensure_populated()
 
-    def enrich(self, content):
+    def enrich(self, content, maxwidth=None, maxheight=None):
         """
         Returns an enriched version of content that replaces all URLs that
         have a provider with valid resource data
@@ -41,7 +39,7 @@ class Consumer(object):
 
             # This is generally a safeguard against bad provider implementations
             try:
-                resource = provider.get_resource(url, maxwidth=self.maxwidth, maxheight=self.maxheight)
+                resource = provider.get_resource(url, maxwidth=maxwidth, maxheight=maxheight)
             except:
                 logger.exception('Failed to get resource from provider %s' % provider)
             else:
@@ -52,13 +50,13 @@ class Consumer(object):
                 content = content.replace(url, resource.render())
         return content
 
-    def devour(self, content=None):
+    def devour(self, content=None, maxwidth=None, maxheight=None):
         """
         Consumes all OEmbed content URLs in the content. Returns a new
         version of the content with URLs replaced with rich content
         """
         pre_consume.send(sender=self)
-        content = self.enrich(content or '')
+        content = self.enrich(content or '', maxwidth=maxwidth, maxheight=maxheight)
         post_consume.send(sender=self)
         return content
 
@@ -92,40 +90,39 @@ class HTMLConsumer(Consumer):
 
 
 def devour(content, html=False, maxwidth=None, maxheight=None, skip_internal=False):
+    """
+    Consume a string interpreting as text or HTML with optional max width/height.
+    Optionally indicate if internal providers should be skipped
+    """
     if html:
         c = HTMLConsumer(skip_internal=skip_internal)
     else:
         c = Consumer(skip_internal=skip_internal)
 
-    c.maxwidth = maxwidth
-    c.maxheight = maxheight
-
-    return c.devour(content)
+    return c.devour(content, maxwidth=maxwidth, maxheight=maxheight)
 
 
 def prefetch(content, html=False, sizes=None):
     """
-    A wrapper for devour() that does specialized work for prefetching
-    OEmbed content. Specify a list of sizes to prefetch. By default,
-    no explicit size will be passed to the providers. Sizes can be a list
-    of two-tuples that are explicit sizes or integers that are converted
-    to size combinations
+    Specify a list of sizes to prefetch. By default, no explicit size will be
+    passed to the providers. Sizes can be a list of two-tuples that are explicit
+    sizes or integers that are converted to size combinations. All internal
+    providers are skipped here if they are not cached
     """
     logger.debug('Prefetching OEmbed content excluding uncached internal providers')
-    devour(content, html=html, skip_internal=True)
 
-    if not sizes:
-        return
+    # Get a consumer
+    c = HTMLConsumer(skip_internal=True) if html else Consumer(skip_internal=True)
+    c.devour(content)
 
-    for size in sizes:
-        logger.debug('Prefetching Size %s' % (size,))
-
+    # Process size combinations
+    for size in (sizes or []):
         # Explicit size
         if isinstance(size, tuple):
-            devour(content, html=html, maxwidth=size[0], maxheight=size[1], skip_internal=True)
+            c.devour(content, maxwidth=size[0], maxheight=size[1])
 
         # All size combinations - (size, None), (None, size), (size, size)
         elif isinstance(size, int):
-            devour(content, html=html, maxwidth=size, skip_internal=True)
-            devour(content, html=html, maxheight=size, skip_internal=True)
-            devour(content, html=html, maxwidth=size, maxheight=size, skip_internal=True)
+            c.devour(content, maxwidth=size)
+            c.devour(content, maxheight=size)
+            c.devour(content, maxwidth=size, maxheight=size)
